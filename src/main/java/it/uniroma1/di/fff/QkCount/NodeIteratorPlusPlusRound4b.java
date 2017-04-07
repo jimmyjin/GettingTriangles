7954ad195cada255c53df171fb2832eb0d1119e8
jimmyjin <jeff_jsm@hotmail.com>
2017-04-06 22:52:57 -0400

    test

--- src/main/java/it/uniroma1/di/fff/QkCount/NodeIteratorPlusPlusRound4b.java
+++ src/main/java/it/uniroma1/di/fff/QkCount/NodeIteratorPlusPlusRound4b.java
@@ -215,6 +215,7 @@ public class NodeIteratorPlusPlusRound4b extends AbstractRound implements Tool {
 				Iterator<Text> it2 = values.iterator();
 				while (it2.hasNext()){
 					Text next = it2.next();
+					context.write(key, next);
 					if (next.toString().equals(QkCountDriver.EDGE_EXISTS_MARKER)) continue;
 					else {
 						String[] nbs = key.toString().split(QkCountDriver.NEIGHBORLIST_SEPARATOR);
